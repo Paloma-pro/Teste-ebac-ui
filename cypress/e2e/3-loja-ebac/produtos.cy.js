0c1137d5ee1bcb3b05173ca61387cd04d1c11861
Paloma <paloma166.pah@gmail.com>
2025-02-10 16:03:38 -0300

    Page Objects + Massa de dados

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -10,21 +10,36 @@ describe('Funcionalidade: Produtos', () => {
 
     it('Deve selecionar um produto da lista', () => {
         produtosPage.buscarProdutoLista('Aero Daily Fitness Tee')
-
             cy.get('.woocommerce-tabs').should('contain', 'Descrição')
     });
 
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         let produto = 'Zoltan Gym Tee'
         produtosPage.buscarProduto(produto)
         cy.get('.product_title').should('contain', produto)
     });
 
     it('Deve visitar a página do produto', () => {
-        
+        produtosPage.visitarProduto('Zoltan Gym Tee')
+        cy.get('.product_title').should('contain', 'Zoltan Gym Tee')
     });
 
     it('Deve adicionar produto ao carrinho', () => {
-        
+        let qtb = 2
+        produtosPage.buscarProduto('Ariel Roll Sleeve Sweatshirt')
+        produtosPage.addProdutoCarrinho('L', 'Red', qtb)
+        cy.get('.woocommerce-message').should('contain', qtb + ' × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.')
     });
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPage.buscarProduto(dados[0].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+                dados[0].tamanho,
+                dados[0].cor,
+                dados[0].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+
+        })
+   });
 });
--- cypress/support/page-objects/produtos.page.js
+++ cypress/support/page-objects/produtos.page.js
@@ -16,11 +16,18 @@ class ProdutosPage {
 
     }
 
-    visitarProduto() {
+    visitarProduto(nomeProduto) {
+        //cy.visit(`produtos/${nomeProduto}`)
 
+        const urlFormatada = nomeProduto.replace(/ /g, '-')
+        cy.visit(`produtos/${urlFormatada}`)
     }
 
-    addProdutoCarrinho() {
+    addProdutoCarrinho(tamanho, cor, quantidade) {
+        cy.get('.button-variable-item-' + tamanho).click()     //Existem dois jeitos de colocar o parâmetro. Esse é o primeiro.
+        cy.get(`.button-variable-item-${cor}`).click()       // E esse é o segundo.
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
 
     }
 }
