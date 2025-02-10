6135ee9f658cdf1338cc93b7ff4c7bd109f318a4
Paloma <paloma166.pah@gmail.com>
2025-02-10 17:27:28 -0300

    Mudança no gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "j4m4gu",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    video: true,
   },
 });
